ee68d42be0b0cd2fc150c969ee3c284cd5d26b60
RaphaelT <raphael.tetreault@mohawkcollege.ca>
2025-10-21 17:48:27 -0400

    Add project and sample

---
+++ physics-drop-ball/Game.cs
@@ -0,0 +1,67 @@
+﻿// Include the namespaces (code libraries) you need below.
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace MohawkGame2D
+{
+    /// <summary>
+    ///     Your game code goes inside this class!
+    /// </summary>
+    public class Game
+    {
+        Vector2 position;
+        Vector2 velocity;
+        float radius = 35;
+        Color color = Color.Red;
+
+        public void Setup()
+        {
+            Window.SetTitle("Motion with Vectors");
+            Window.SetSize(400, 800);
+
+            // Set up variables once game is ready
+            position = new(Window.Width / 2, 100);
+            
+            // 
+            Draw.LineSize = 1;
+        }
+
+        public void Update()
+        {
+            Window.ClearBackground(Color.OffWhite);
+
+            // Run game logic
+            ApplyGravity();
+            ConstrainBallToScreen();
+
+            // Draw ball
+            Draw.FillColor = color;
+            Draw.Circle(position, radius);
+        }
+
+        void ApplyGravity()
+        {
+            // Apply gravity to velocity
+            velocity += new Vector2(0, 10) * Time.DeltaTime;
+            // Apply velocity to position
+            position += velocity;
+        }
+
+        void ConstrainBallToScreen()
+        {
+            // Is bottom of ball at or past edge of screen
+            if (position.Y + radius >= Window.Height)
+            {
+                // Inverse Y velocity, move upwards
+                velocity.Y = -velocity.Y;
+                // Slow down velocity
+                velocity *= 0.8f; // retain 80% of velocity
+                // Place ball against bottom edge of screen
+                position.Y = Window.Height - radius;
+            }
+        }
+
+    }
+
+}
